9000f73f76d7923cdc5da86e4a85fc263327fea0
Abdulloh20080825 <abdullohkurbonov2008@gmail.com>
2024-10-13 13:27:53 +0500

    as

--- store/slice/filterByCategory.ts
+++ store/slice/filterByCategory.ts
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const tfilterByCategories = createAsyncThunk(
+export const filterByCategories = createAsyncThunk(
 	'filterByCategories/filterByCategories',
 	async (id: string) => {
 		const response = await axios.get(
